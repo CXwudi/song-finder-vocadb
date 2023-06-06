b33857a1f2630aa3e883d21c006f685bf3004b12
CXwudi <cx2010017@gmail.com>
2023-06-05 22:29:07 -0400

    some refactor :recycle:

--- songfinder-app/src/main/kotlin/mikufan/cx/songfinder/Entrypoint.kt
+++ songfinder-app/src/main/kotlin/mikufan/cx/songfinder/Entrypoint.kt
@@ -1,14 +1,19 @@
 package mikufan.cx.songfinder
 
+import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
 import androidx.compose.material.Surface
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import mikufan.cx.songfinder.model.IOFiles
 import mikufan.cx.songfinder.ui.component.LoadingScreen
 import mikufan.cx.songfinder.ui.theme.MyAppTheme
 
@@ -18,17 +23,42 @@ import mikufan.cx.songfinder.ui.theme.MyAppTheme
  */
 
 fun main() = application {
-  var initialLoaded by remember { mutableStateOf(false) }
+  var targetFiles: IOFiles? by remember { mutableStateOf(null) }
 
-  if (!initialLoaded) {
+  if (targetFiles == null) {
     Window(
       onCloseRequest = ::exitApplication,
-      title = "Loading...",
+      title = "Loading Input and Output Files",
     ) {
-      MyAppTheme {
-        Surface(modifier = Modifier.fillMaxSize()) {
-          LoadingScreen(onStarted = {})
-        }
+      MyApp {
+        LoadingScreen(onStarted = {})
+      }
+    }
+  }
+}
+
+@Composable
+fun MyApp(
+  content: @Composable () -> Unit,
+) {
+  MyAppTheme {
+    Surface(modifier = Modifier.fillMaxSize()) {
+      content()
+    }
+  }
+}
+
+@Preview
+@Composable
+fun previewOfRoot() {
+  MyApp {
+    Column(
+      horizontalAlignment = Alignment.CenterHorizontally,
+      verticalArrangement = Arrangement.spacedBy(5.dp),
+    ) {
+      Text("Hello World")
+      Button(onClick = {}) {
+        Text("Click me")
       }
     }
   }
---
+++ songfinder-app/src/main/kotlin/mikufan/cx/songfinder/model/IOFiles.kt
@@ -0,0 +1,12 @@
+package mikufan.cx.songfinder.model
+
+import java.nio.file.Path
+
+/**
+ * @author CX无敌
+ * 2023-06-05
+ */
+data class IOFiles(
+  val inputTxt: Path,
+  val outputCSV: Path,
+)
