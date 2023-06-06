d77b0858038a2f6682aef9ac92d63947f7849295
CXwudi <cx2010017@gmail.com>
2023-06-05 22:41:38 -0400

    :sparkles: beautiful theme

--- songfinder-app/src/main/kotlin/mikufan/cx/songfinder/ui/theme/Theme.kt
+++ songfinder-app/src/main/kotlin/mikufan/cx/songfinder/ui/theme/Theme.kt
@@ -1,39 +1,51 @@
 package mikufan.cx.songfinder.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.shape.CutCornerShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Shapes
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 private val LightColorPalette = lightColors(
-  primary = Color.Blue,
-  primaryVariant = Color.Blue,
-  secondary = Color.Blue,
+  primary = Color(0xFF6200EE),
+  primaryVariant = Color(0xFF3700B3),
+  secondary = Color(0xFF03DAC6),
   background = Color.White,
   surface = Color.White,
   onPrimary = Color.White,
-  onSecondary = Color.White,
+  onSecondary = Color.Black,
   onBackground = Color.Black,
   onSurface = Color.Black,
 )
+
 private val DarkColorPalette = darkColors(
-  primary = Color.Blue,
-  primaryVariant = Color.Blue,
-  secondary = Color.Blue,
-  background = Color.Black,
-  surface = Color.Black,
-  onPrimary = Color.White,
-  onSecondary = Color.White,
+  primary = Color(0xFFBB86FC),
+  primaryVariant = Color(0xFF3700B3),
+  secondary = Color(0xFF03DAC6),
+  background = Color(0xFF121212),
+  surface = Color(0xFF121212),
+  onPrimary = Color.Black,
+  onSecondary = Color.Black,
   onBackground = Color.White,
   onSurface = Color.White,
+)
+
+private val shapes = Shapes(
+  small = RoundedCornerShape(4.dp),
+  medium = RoundedCornerShape(8.dp),
+  large = CutCornerShape(topStart = 16.dp, bottomEnd = 16.dp),
 )
 
 @Composable
 fun MyAppTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
   MaterialTheme(
     colors = if (darkTheme) DarkColorPalette else LightColorPalette,
+    shapes = shapes,
     content = content,
   )
 }
