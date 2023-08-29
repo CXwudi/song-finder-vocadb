6206c6a85ae0195f3efc8dc461fa33fb983b6178
CXwudi <cx2010017@gmail.com>
2023-08-29 18:12:26 +0800

    :test_tube: Added TestFilePicker.kt for UI testing.
    A new file, TestFilePicker.kt has been added under the testing directory of the songfinder UI module. This test file allows us to interactively test the file and directory picker functionality of the application. The implementation for the file and directory pickers has been adapted from the sample code provided in the 'darkrockstudios' library repository. A UI to choose file/directory and display the chosen path has been set up.

---
+++ songfinder-app/src/main/kotlin/mikufan/cx/songfinder/ui/test/TestFilePicker.kt
@@ -0,0 +1,52 @@
+package mikufan.cx.songfinder.ui.test
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+import com.darkrockstudios.libraries.mpfilepicker.DirectoryPicker
+import com.darkrockstudios.libraries.mpfilepicker.FilePicker
+
+
+fun main() = application {
+  var show by remember { mutableStateOf(false) }
+  var pathChosen by remember { mutableStateOf("") }
+
+  var showDirPicker by remember { mutableStateOf(false) }
+  var dirChosen by remember { mutableStateOf("") }
+
+  Window(onCloseRequest = ::exitApplication) {
+    Column {
+      Button(onClick = {
+        show = true
+      }) {
+        Text("Choose File")
+      }
+      Text("File Chosen: $pathChosen")
+
+      /////////////////////////////////////////////////////////////////
+
+      Button(onClick = {
+        showDirPicker = true
+      }) {
+        Text("Choose Directory")
+      }
+      Text("Directory Chosen: $dirChosen")
+    }
+  }
+
+  FilePicker(show, fileExtensions = listOf("jpg", "png")) { file ->
+    pathChosen = file?.path ?: "none selected"
+    show = false
+  }
+
+  DirectoryPicker(showDirPicker) { path ->
+    dirChosen = path ?: "none selected"
+    showDirPicker = false
+  }
+}
