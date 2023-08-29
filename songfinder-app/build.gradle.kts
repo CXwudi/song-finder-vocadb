15e57ed24372e87f24a4e049ba708147b8b7cdb0
CXwudi <cx2010017@gmail.com>
2023-08-29 23:08:57 +0800

    added DB connection

--- songfinder-app/build.gradle.kts
+++ songfinder-app/build.gradle.kts
@@ -2,8 +2,13 @@ plugins {
   id("my.compose-desktop-app")
 }
 
+dependencies {
+  implementation("org.springframework.boot:spring-boot-starter-jdbc")
+  runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
+}
+
 compose.desktop {
   application {
-    mainClass = "mikufan.cx.songfinder.TestMainKt"
+    mainClass = "mikufan.cx.songfinder.EntrypointKt"
   }
 }
