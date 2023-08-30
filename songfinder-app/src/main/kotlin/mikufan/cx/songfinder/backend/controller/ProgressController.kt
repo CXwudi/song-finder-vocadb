9f19696aed97b7f2b81eb540537832a74164f135
CXwudi <cx2010017@gmail.com>
2023-08-30 18:24:44 +0800

    :recycle: Refactor ProgressController for improved readability
    The ProgressController in the SongFinder backend has been refactored to improve readability and accessibility. Instead of using a function to retrieve currentCountState, it is now accessed as a property. The property totalCount is also added for more comprehensive progress tracking.

--- songfinder-app/src/main/kotlin/mikufan/cx/songfinder/backend/controller/ProgressController.kt
+++ songfinder-app/src/main/kotlin/mikufan/cx/songfinder/backend/controller/ProgressController.kt
@@ -10,5 +10,8 @@ class ProgressController(
   private val inputFileLineReader: InputFileLineReader,
 ) {
 
-  fun getCurrentCountState() = progressTracker.currentCountState
+  val currentCountState
+    get() = progressTracker.currentCountState
+
+  val totalCount = progressTracker.totalCount
 }
