dd6ca28c27c03c3980367c481a66d3a7ba90ed9e
CXwudi <cx2010017@gmail.com>
2023-10-12 19:15:11 +0800

    :checkered_flag: Add TODO, continue next time

--- songfinder-app/src/main/kotlin/mikufan/cx/songfinder/ui/component/main/SearchPanel.kt
+++ songfinder-app/src/main/kotlin/mikufan/cx/songfinder/ui/component/main/SearchPanel.kt
@@ -2,8 +2,12 @@ package mikufan.cx.songfinder.ui.component.main
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import mikufan.cx.songfinder.SpringCtx
+import mikufan.cx.songfinder.backend.controller.IOController
+import org.springframework.beans.factory.getBean
 
 @Composable
 fun SearchPanel(modifier: Modifier = Modifier) {
-
+  SpringCtx.current.getBean<IOController>()
+  // TODO: implement SearchPanel
 }
