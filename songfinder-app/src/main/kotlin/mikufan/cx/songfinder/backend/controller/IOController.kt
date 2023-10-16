4e719e287739d3b9a1c3058fdef19b3d66903b31
CXwudi <cx2010017@gmail.com>
2023-10-17 00:00:58 +0800

    :sparkles: Add state handling for next line read in IOController
    In order to improve the maintainability and scalability of the 'IOController', a private lazy 'currentLineState' has been added to manage state. The existing 'readNextLine()' method has been updated to 'readNextLineToState()' which now utilizes the new state variable for line reads. The change contributes to easier debugging and better handling of I/O operations.

--- songfinder-app/src/main/kotlin/mikufan/cx/songfinder/backend/controller/IOController.kt
+++ songfinder-app/src/main/kotlin/mikufan/cx/songfinder/backend/controller/IOController.kt
@@ -1,5 +1,7 @@
 package mikufan.cx.songfinder.backend.controller
 
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
 import mikufan.cx.songfinder.backend.component.InputFileLineReader
 import mikufan.cx.songfinder.backend.component.OutputCsvLineWriter
 import mikufan.cx.songfinder.backend.component.ProgressTracker
@@ -12,7 +14,13 @@ class IOController(
   private val progressTracker: ProgressTracker,
 ) {
 
-  fun readNextLine() = inputFileLineReader.readNext()
+  private val _currentLineState by lazy { mutableStateOf(inputFileLineReader.readNext() ?: "") }
+
+  val currentLineState: State<String> get() = _currentLineState
+
+  fun readNextLineToState() {
+    _currentLineState.value = inputFileLineReader.readNext() ?: throw IllegalStateException("No more lines to read")
+  }
 
   fun writeSongIdAndIncrementIndex(songId: ULong) {
     outputCsvLineWriter.writeSongId(songId)
