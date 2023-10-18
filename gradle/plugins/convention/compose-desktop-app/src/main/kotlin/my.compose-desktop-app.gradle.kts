15b0dc81f37bef2f068a71767621e712e8821e0b
CXwudi <cx2010017@gmail.com>
2023-10-19 00:17:52 +0800

    :heavy_plus_sign: Add "-Xjvm-default=all" to compilerArgs in gradle config
    This commit changes the compiler options `freeCompilerArgs` in the compose-desktop gradle configuration. In addition to existing "-Xjsr305=strict" flag, a new flag "-Xjvm-default=all" is added. This enables JVM default behavior, which might be useful in certain cases.

--- gradle/plugins/convention/compose-desktop-app/src/main/kotlin/my.compose-desktop-app.gradle.kts
+++ gradle/plugins/convention/compose-desktop-app/src/main/kotlin/my.compose-desktop-app.gradle.kts
@@ -23,7 +23,7 @@ kotlin {
     compilations.all {
       kotlinOptions {
         javaParameters = true // see the same reason in jvm-root mixin
-        freeCompilerArgs = freeCompilerArgs + listOf("-Xjsr305=strict") // enable strict null check
+        freeCompilerArgs = freeCompilerArgs + listOf("-Xjsr305=strict", "-Xjvm-default=all") // enable strict null check + jvm default
         // kotlin will use a java tool chain version, so no need to specify a java version here
         // see https://kotlinlang.org/docs/gradle-configure-project.html#gradle-java-toolchains-support
       }
