07a89f6ac283866286875bccb30112e710cdf1a9
CXwudi <cx2010017@gmail.com>
2024-01-01 17:35:14 -0500

    :memo: add comments

--- songfinder-app/src/main/kotlin/mikufan/cx/songfinder/ui/common/CustomLayout.kt
+++ songfinder-app/src/main/kotlin/mikufan/cx/songfinder/ui/common/CustomLayout.kt
@@ -17,23 +17,31 @@ fun ColumnThatResizesFirstItem(
   resizibleFirstContent: @Composable () -> Unit,
   fixedSizeSecondContent: @Composable () -> Unit
 ) {
+  // SubcomposeLayout allows us to measure and place child layouts manually
   SubcomposeLayout(modifier = modifier) { constraints ->
+    // Measure the second item first to ensure it's always visible
     val fixedSizePlacebles = subcompose(Phase.One, fixedSizeSecondContent).map {
       it.measure(constraints.copy(minHeight = 0, minWidth = 0))
     }
+    // Calculate the remaining height after placing the second item
     val firstContentMaxHeight = fixedSizePlacebles.maxOfOrNull { it.height } ?: 0
     val remainingHeight: Int = constraints.maxHeight - firstContentMaxHeight
 
+    // Measure the first item with the remaining height
     val resiziblePlacebles = subcompose(Phase.Two, resizibleFirstContent).map {
       it.measure(constraints.copy(minHeight = 0, maxHeight = remainingHeight))
     }
 
+    // Calculate the y position for placing the first item
     val yPosition = resiziblePlacebles.maxOfOrNull { it.height } ?: 0
 
+    // Layout the items
     layout(constraints.maxWidth, constraints.maxHeight) {
+      // Place the first item at the top
       resiziblePlacebles.forEach { placeable ->
         placeable.placeRelative(0, 0)
       }
+      // Place the second item below the first item
       fixedSizePlacebles.forEach { placeable ->
         placeable.placeRelative(0, yPosition)
       }
