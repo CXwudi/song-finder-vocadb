688ac95b8f882ba3be2998b896952371b140ba38
CXwudi <cx2010017@gmail.com>
2025-03-18 23:23:07 -0400

    :sparkles: Add new virtual singer types
    Expand ArtistType enum with VOICEVOX, ACEVirtualSinger, and AIVOICE, and update isVirtualSinger() function

--- songfinder-app/src/main/kotlin/mikufan/cx/songfinder/backend/db/entity/ArtistType.kt
+++ songfinder-app/src/main/kotlin/mikufan/cx/songfinder/backend/db/entity/ArtistType.kt
@@ -4,11 +4,12 @@ enum class ArtistType {
   Unknown, Circle, Label, Producer, Animator, Illustrator, Lyricist, Vocaloid, UTAU, CeVIO,
   OtherVoiceSynthesizer, OtherVocalist, OtherGroup, OtherIndividual, Utaite,
   Band, Vocalist, Character, SynthesizerV, CoverArtist, NEUTRINO,
-  VoiSona, NewType, Voiceroid, Instrumentalist, Designer;
+  VoiSona, NewType, Voiceroid, Instrumentalist, Designer,
+  VOICEVOX, ACEVirtualSinger, AIVOICE;
 
   fun isVirtualSinger() = this in setOf(
     Vocaloid, UTAU, Utaite, CeVIO, OtherVoiceSynthesizer, SynthesizerV, NEUTRINO,
-    VoiSona, NewType, Voiceroid
+    VoiSona, NewType, Voiceroid, VOICEVOX, ACEVirtualSinger, AIVOICE
   )
 
   fun isVocalist() = this in setOf(
